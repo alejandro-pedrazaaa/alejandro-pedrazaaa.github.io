9ee33fe909d2d67a7a3c9ef1b91f8e1a672f5949
Alejandro Pedraza <alepedpon1@gmail.com>
2022-12-20 14:12:06 -0600

    finish

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -1,37 +1,41 @@
 export const ContactMe = () => {
   return (
-    <footer className="container-fluid" id="contact-me">
-      <div className="row">
-        <div className="col-12 contact-me">
-          <h2>Contact Me</h2>
-          <div className="col-12">
-            <p>
-              <i className="fa-solid fa-phone"></i> Phone: 123-456-7890
+    <footer className="contact-me" id="contact-me">
+      <div className="container ">
+        <div className="row text-center px-0">
+          <div className="col-12 px-0">
+            <h2>Contact Me</h2>
+            <div className="col-12">
+              <p>
+                <i className="fa-solid fa-phone"></i> Phone: 479-466-3167
+              </p>
+            </div>
+            <div className="col-12">
+              <p>
+                <i className="fa-solid fa-envelope"></i> Email:
+                alepedpon1@gmail.com
+              </p>
+            </div>
+          </div>
+          <div className="col-12 px-0 my-4">
+            <h2>Follow Me</h2>
+            <div className="col-12 d-flex justify-content-evenly">
+              <a href="https://www.instagram.com/alejandro_pedrazaaa">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+              <a href="https://github.com/alejandro-pedrazaaa">
+                <i className="fa-brands fa-github"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/alejandro-pedrazaaa/">
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div className="col-12  px-0 pt-2">
+            <p className="copyright">
+              Copyright &copy; 2022 alejandropedrazaaa.com
             </p>
           </div>
-          <div className="col-12">
-            <p>
-              <i className="fa-solid fa-envelope"></i> Email:
-              contact@alejandropedrazaaa.com
-            </p>
-          </div>
-        </div>
-        <div className="col-12 follow-me">
-          <h2>Follow Me</h2>
-          <div className="col-12">
-            <a>
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-            <a>
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a>
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </div>
-        </div>
-        <div className="col-12 copyright">
-          <p>Copyright &copy; 2022 alejandropedrazaaa.com</p>
         </div>
       </div>
     </footer>
