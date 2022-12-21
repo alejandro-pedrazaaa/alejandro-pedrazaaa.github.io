3bfb8ac279c36218b1d1dae316fb800b0c2698cd
Alejandro Pedraza <alepedpon1@gmail.com>
2022-12-21 17:52:14 -0600

    fix h1

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ export const AboutMe = () => {
         <div className="row">
           <div className="px-0 col-12">
             <h1 className="my-5">
-              UX/UI Designer <br></br> & <br></br> Frontend Developer
+              UX/UI Designer <br></br> & <br></br> Front-end Developer
             </h1>
             <p className="mb-5">
               👋 Hey there, I am Alejandro! <br></br> I design and develop
